ce109f0a5fdf1f9b74b94fc76a59034c863a488d
Dikshant Koriwar <dikshantkoriwar4@gmail.com>
2025-03-18 23:32:54 +0530

    1  day

---
+++ Flowchart & Pseudocode/A1.java
@@ -0,0 +1,29 @@
+
+public class A1 {
+   public static void main(String[] args) {
+      // int p = 10000;
+      // float r = 8f;
+      // int t = 2;
+      // float simpleInterest = (p * r * t) / 100;
+      // System.out.println("Simple Interest :" + simpleInterest);
+
+
+
+      //max in 3 nuber
+      // int a=0,b=34,c=98;
+      // if(a > b){
+      //    System.out.println("a "+a);
+      // }else if(a > c){
+      //    System.out.println("a "+a);
+      // }else if(b > c){
+      //    System.out.println("b "+b);
+      // }else{
+      //    System.out.println("c "+c);
+      // }
+
+      //find even or odd number --- very easy 
+
+      //sum of 1 to N natural number ---very easy
+   }
+
+}
---
+++ Orientation Session/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("firt Class of Orientation on 18/03/2025");
+
+    }
+}
