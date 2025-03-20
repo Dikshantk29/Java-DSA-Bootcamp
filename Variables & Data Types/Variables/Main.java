3d23313ab4c461a408515aa7e63384c450a8dbb2
Dikshant Koriwar <dikshantkoriwar4@gmail.com>
2025-03-20 21:01:56 +0530

    variables and datatypes

---
+++ Variables & Data Types/Variables/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args) {
+        
+        //class -blueprint
+        //main() ->starting point of the code
+        //all the code you should write in inside the main()
+
+        System.out.println("Hello World");// ln represent next line
+
+        System.out.print("hello World");// print on the same line
+
+        //dattypes
+        //int a=3;
+
+    }
+}
