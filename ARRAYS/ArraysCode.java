6b790161138b2e09fd40214f591c59ce4809efa3
Dikshant Koriwar <dikshantkoriwar4@gmail.com>
2025-04-03 21:40:29 +0530

    Arrays

---
+++ ARRAYS/ArraysCode.java
@@ -0,0 +1,9 @@
+package ARRAYS;
+public class ArraysCode {
+    public static void main(String args[]) {
+
+        //arrays concepts
+        
+
+    }
+}
--- Operator/Class.java
+++ Operator/Class.java
@@ -2,6 +2,7 @@ public class Class {
     public static void main(String[] args) {
 
         int a = 10;
+    
         System.out.println(a++);
         int b = 10;
         System.out.println(++b);
