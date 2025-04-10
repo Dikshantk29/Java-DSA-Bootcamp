ca4f1c21b728b53cf423227590186c29e6683a57
Dikshant Koriwar <dikshantkoriwar4@gmail.com>
2025-04-10 10:42:09 +0530

    2D array

---
+++ 2D_Array/SpiralMatrix.java
@@ -0,0 +1,51 @@
+public class SpiralMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {
+               {1,2,3,4},
+               {5,6,7,8},
+               {9,10,11,12},
+               {13,14,15,16}
+        };
+        spiralPrint(arr);
+    }
+
+    static void spiralPrint(int[][] arr) {
+        int rows = arr.length;
+        int cols = arr[0].length;
+
+        int startRow = 0, endRow = rows - 1;
+        int startCol = 0, endCol = cols - 1;
+
+        while (startRow <= endRow && startCol <= endCol) {
+            // top
+            for (int j = startCol; j <= endCol; j++) {
+                System.out.print(arr[startRow][j] + " ");
+            }
+
+            // right
+            for (int i = startRow + 1; i <= endRow; i++) {
+                System.out.print(arr[i][endCol] + " ");
+            }
+
+            // bottom
+            if (startRow < endRow) {
+                for (int j = endCol - 1; j >= startCol; j--) {
+                    System.out.print(arr[endRow][j] + " ");
+                }
+            }
+
+            // left
+            if (startCol < endCol) {
+                for (int i = endRow - 1; i > startRow; i--) {
+                    System.out.print(arr[i][startCol] + " ");
+                }
+            }
+
+            startRow++;
+            endRow--;
+            startCol++;
+            endCol--;
+        }
+        System.out.println();
+    }
+}
