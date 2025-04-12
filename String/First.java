afb9799b7885611e88ada9a6a093bf915b655052
Dikshant Koriwar <dikshantkoriwar4@gmail.com>
2025-04-12 09:03:12 +0530

    String

---
+++ String/First.java
@@ -0,0 +1,13 @@
+package String;
+
+public class First {
+   public static void main(String[] args) {
+    char[] str={'a','b','c'};
+    String str1="abc";
+    String str2= new String("abc");
+    //as programer is same
+
+
+    
+   } 
+}
