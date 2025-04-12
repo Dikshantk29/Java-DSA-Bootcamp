01af30651edc42d90e1fe9a9693ed4f8a2ed126e
Dikshant Koriwar <dikshantkoriwar4@gmail.com>
2025-04-12 09:14:16 +0530

    String

---
+++ 10_String/First.java
@@ -0,0 +1,21 @@
+package String;
+
+public class First {
+    public static void main(String[] args) {
+        char[] str = { 'a', 'b', 'c' };
+        String str1 = "abc";
+        String str2 = new String("abc");
+        // as programer is same
+        // string are immutable
+
+        // concatination
+        String str3 = str1 + str2;
+        System.out.println(str3);
+
+        System.out.println(str1.concat(str2));
+
+
+        //charAt
+        System.out.println(str1.charAt(0));
+    }
+}
