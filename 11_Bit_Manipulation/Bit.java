5fc55714cc666fcceb9aedabf064ebc63978af01
Dikshant Koriwar <dikshantkoriwar4@gmail.com>
2025-04-16 15:01:36 +0530

    Bit Manipulation

---
+++ 11_Bit_Manipulation/Bit.java
@@ -0,0 +1,17 @@
+
+public class Bit {
+    public static void main(String[] args) {
+
+        /*
+         * Bitwise operator we have
+         * binary and &
+         * binary or |
+         * binary xor ^
+         * binary not ~
+         * left shift <<
+         * right shift >>
+         * unsigned right shift >>
+         */
+        System.out.println((~(5)) & 1);
+    }
+}
