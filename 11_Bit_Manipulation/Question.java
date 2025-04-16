ffe5d1bd28ab6fad9544e7dff046a9ea28993f3d
Dikshant Koriwar <dikshantkoriwar4@gmail.com>
2025-04-16 21:53:10 +0530

    Bit Manipulation

--- 11_Bit_Manipulation/Question.java
+++ 11_Bit_Manipulation/Question.java
@@ -1,17 +1,17 @@
-public class Question {
+public class Question{ 
+    public static void main(String[] args){
 
-    public static void main(String[] args) {
         // find number is even or odd
         int number = 10;
         if ((number & 1) == 0) {
             System.out.println("Even");
-        } else {
+        }else {
             System.out.println("Odd");
         }
-
+                               
         // find number is power of 2
         int number1 = 16;
-        if ((number1 & (number1 - 1)) == 0) {
+        if((number1 & (number1 - 1)) == 0) {
             System.out.println("Power of 2");
         } else {
             System.out.println("Not Power of 2");
@@ -30,7 +30,7 @@ public class Question {
         System.out.println(setBit);
 
         // clear ith bit of number
-        int number4 = 10;
+        int number4 = 10; 
         int i2 = 2;
         int clearBit = number4 & ~(1 << i2);
         System.out.println(clearBit);
@@ -50,7 +50,7 @@ public class Question {
         // count number of set bits
         int number6 = 10;
         int count = 0;
-        while (number6 > 0) {
+        while (number6 > 0){
             count += number6 & 1;
             number6 >>= 1;
         }
@@ -71,7 +71,7 @@ public class Question {
         a = a ^ b;
         b = a ^ b;
         a = a ^ b;
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        System.out.println("a = "+ a);
+        System.out.println("b = "+ b);
     }
 }
