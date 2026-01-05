a302820110f291b3d9d2ae674e48824d90f7cba7
Dikshant Koriwar <dikshantkoriwar4@gmail.com>
2026-01-05 15:58:21 +0530

    arraylist problem

---
+++ 17_ArrayList/ContainWithMostWater.java
@@ -0,0 +1,40 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ContainWithMostWater {
+    public static void main(String[] args) {
+        ArrayList<Integer> height = new ArrayList<>(Arrays.asList(1, 8, 6, 2, 5, 4, 8, 3, 7));
+        System.out.println(maxArea(height));
+        System.out.println(maxAreaOptimal(height));
+    }
+
+    // brute force approach O(n^2)
+    public static int maxArea(ArrayList<Integer> height) {
+        int maxArea = 0;
+        for (int i = 0; i < height.size(); i++) {
+            for (int j = i + 1; j < height.size(); j++) {
+                int area = Math.min(height.get(i), height.get(j)) * (j - i);
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+        return maxArea;
+    }
+
+    // optimal approach O(n) 2 pointer approach
+    public static int maxAreaOptimal(ArrayList<Integer> height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.size() - 1;
+        while (left < right) {
+            int area = Math.min(height.get(left), height.get(right)) * (right - left);
+            maxArea = Math.max(maxArea, area);
+            if (height.get(left) < height.get(right)) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
