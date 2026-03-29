0c1131f171f5e40d28c4031d9ff8b7e66f9ce344
Dikshant Koriwar <dikshantkoriwar4@gmail.com>
2026-03-29 20:37:47 +0530

    feat greedy approach

--- 21_Greeedy_Approach/ActivitySelection.java
+++ 21_Greeedy_Approach/ActivitySelection.java
@@ -1,33 +1,51 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class ActivitySelection {
-    // disjoint set of activities
-    // max meeting in a room are the same problem
     public static void main(String[] args) {
-      
 
         int[] start = { 1, 3, 0, 5, 8, 5 };
         int[] end = { 2, 4, 6, 7, 9, 9 };
-        
 
-        //end time basis sorted
-        int maxAct =0;
+        ////sorting 
+        int[][] activites = new int[start.length][3];
+        for (int i = 0; i < start.length; i++) {
+            activites[i][0] = i;
+            activites[i][1] = start[i];
+            activites[i][2] = end[i];
+
+        }
+        ////lamda expression
+        Arrays.sort(activites, Comparator.comparingDouble(o -> o[2]));
+
+        // end time sort basis sorted
+        int maxAct = 0;
         ArrayList<Integer> ans = new ArrayList<>();
 
- //last activity
+        // 1st activity
         maxAct = 1;
         ans.add(0);
+        //ans.add(activites[0][0]);
+
         int lastEnd = end[0];
-        for (int i = 1; i < start.length; i++) {
+        // int lastEnd = activites[0][2];
+        for (int i = 1; i < end.length; i++) {
             if (start[i] >= lastEnd) {
+                // if (activites[i][1] >= lastEnd) {
+
+                // activity select
                 maxAct++;
                 ans.add(i);
+                // ans.add(activites[i][0]);
                 lastEnd = end[i];
+                // lastEnd = activites[i][2];
             }
         }
 
-        System.out.println("Maximum activities: " + maxAct);
-        System.out.println("Selected activities: " + ans);
+        System.out.println("Maximum Activity = " + maxAct);
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.println("A" + ans.get(i) + " ");
+        }
+        System.out.println();
 
     }
 }
