54400de32cc650b935a2e4baea9551c55b3615c7
Dikshant Koriwar <dikshantkoriwar4@gmail.com>
2026-04-23 21:56:13 +0530

    recursion problems

--- 13_Recurssion/Factorial.java
+++ 13_Recurssion/Factorial.java
@@ -2,7 +2,8 @@ public class Factorial {
     public static void main(String[] args) {
       // Factorial of 5 = 5 * 4 * 3 * 2 * 1 = 120
       // Factorial of 0 = 1
-        System.out.println(factorial(5)); // 5! = 120
+        System.out.println("factorial(5): " + factorial(5)); // 5! = 120
+        System.out.println("factorial(0): " + factorial(0)); // 0! = 1
     }
     static int factorial(int n) {
         if (n == 0 || n == 1) {
--- 13_Recurssion/advanced/RemoveDuplicate.java
+++ 13_Recurssion/advanced/RemoveDuplicate.java
@@ -8,14 +8,9 @@ public class RemoveDuplicate {
         boolean[] map = new boolean[26]; // Assuming lowercase letters only
         System.out.println(removeDuplicates(str, newStr, 0, map));
     }
-    // This function removes duplicate characters from a string using recursion.
-
-    // It uses a helper function to keep track of the current index and the result string.
-
-    // The time complexity is O(n^2) because for each character, it checks if it is already in the result string.
-
-    // The space complexity is O(n) for the result string.
-    static String removeDuplicates(String str,StringBuilder newStr, int idx, boolean[] map) {
+   
+    static String removeDuplicates(String str,StringBuilder newStr, 
+        int idx, boolean[] map) {
         if (idx == str.length()) {
             return newStr.toString();
         }
